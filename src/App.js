49cd562b20513ccbbe032bad6466e37f0c58f5d6
Travis Kohlbeck <me+git@travisk.info>
2020-04-05 13:20:35 -0400

    Format and style the text and page layout
    - Install package for code highlighting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header>Hi, you might be able to help me with a problem</header>
+			<p>
+				Here I will explain what I'm building and what I am trying to do. I have
+				a React.js application receiving updates from the Twitch API via
+				websockets (<code>socket.io</code>)
+			</p>
+			<p>
+				Here I will explain the problem, the expected behavior, code, outputs,
+				etc.
+			</p>
+			<p>Make me a code section</p>
+			<div className='code'>
+				<span>JavaScript</span>
+				<SyntaxHighlighter language='javascript' style={docco}>
+					{`let foo = 'bar'`}
+				</SyntaxHighlighter>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
+
+// code highlighting?
